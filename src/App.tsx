cb6620d40a8a63f3ee4b371c007ceb202ed6c58c
Bolo <glorybolo@outlook.com>
2023-03-28 23:12:14 +0900

    using ipfs-core

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,37 @@
-/* src/App.js */
-import './App.css'
-import { useState } from 'react'
-import { create } from 'ipfs-http-client'
-import { Buffer } from 'buffer'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-/* configure Infura auth settings */
-const projectId = process.env.REACT_APP_INFURA_PROJECT_ID
-const projectSecret = process.env.REACT_APP_INFURA_PROJECT_SECRET
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-
-/* create the client */
-const client = create({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https',
-  headers: {
-      authorization: auth,
-  },
-})
+import * as IPFS from 'ipfs-core';
 
 function App() {
-  const [fileUrl, updateFileUrl] = useState(``)
-  async function onChange(e: any) {
-    const file = e.target.files[0]
-    try {
-      const added = await client.add(file)
-      const url = `https://infura-ipfs.io/ipfs/${added.path}`
-      updateFileUrl(url)
-      console.log("IPFS URI: ", url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }  
-  }
+  React.useEffect(() => {
+    const fun = async () => {
+      const ipfs = await IPFS.create({ repo: 'bolo' + Math.random() })
+      const { path } = await ipfs.add('Hello world')
+      console.info("https://ipfs.io/ipfs/" + path)
+    }
+
+    fun();
+  }, [])
   return (
     <div className="App">
-      <h1>IPFS Example</h1>
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      {
-        fileUrl && (
-          <div>
-            <img src={fileUrl} width="600px" alt="file" />
-            <a href={fileUrl} target="_blank" rel="noreferrer noopener">{fileUrl}</a>
-          </div>
-        )
-      }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default App
+export default App;
